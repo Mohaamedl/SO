31abde49d8c1e6e6c2b891cd83d0173e015df8ef
mohamed <mohamedhadodo33@gmail.com>
2023-11-21 12:42:35 +0000

    l6.1

--- aula06/sortNumbersbi.c
+++ aula06/sortNumbersbi.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define LINEMAXSIZE 100
+
 /* SUGESTÂO: utilize as páginas do manual para conhecer mais sobre as funções usadas:
  man qsort
 */
@@ -14,19 +16,44 @@ int compareInts(const void *px1, const void *px2)
 
 int main(int argc, char *argv[])
 {
-    int i, numSize;
+    int i = 0, numSize = 0;
     int *numbers;
+    FILE *fp = NULL;
+    char line[LINEMAXSIZE];
+    fp = fopen(argv[1],"r");
 
-    numSize = argc - 1;
+    if(fp == NULL) 
+    {
+        printf("Can't read the file %s\n", argv[1]);
+        return EXIT_FAILURE;
+    }
+
+    while(fgets(line, sizeof(line),fp) != NULL)
+    {
+        numSize++;
+    }
+
+    fclose(fp);
 
     /* Memory allocation for all the numbers in the arguments */
     numbers = (int *) malloc(sizeof(int) * numSize);
 
-    /* Storing the arguments in the "array" numbers */
-    for(i = 0 ; i < numSize ; i++)
+    fp = fopen(argv[1], "r");
+
+    if(fp == NULL) 
     {
-        numbers[i] = atoi(argv[i+1]);
+        printf("Can't read the file %s\n", argv[1]);
+        return EXIT_FAILURE;
     }
+
+    /* Storing the arguments in the "array" numbers */
+    while(fgets(line, sizeof(line), fp) != NULL)
+    {
+        numbers[i] = atoi(line);
+        i++;
+    }
+
+    fclose(fp);
 
     /* void qsort(void *base, size_t nmemb, size_t size, int (*compar)(const void *, const void *)); 
          The qsort() function sorts an array with nmemb elements of size size.*/
